6064086f76d17ee05cedac0766d21c54c663b324
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-12-22 18:58:09 -0300

    organize folders

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,8 +1,28 @@
+export interface NotifictionProps {
+  content: string;
+  category: string;
+}
+
 export class Notification {
-  private content: string;
-  private category: string;
+  private props: NotifictionProps;
 
   constructor() {
     this.content = '';
   }
+
+  public set content(content: string) {
+    if (content.length < 5) {
+      throw new Error();
+    }
+
+    this.props.content = content;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
 }
+
+const notifiction = new Notification();
+
+notifiction.content = 'asdasdasdsad';
